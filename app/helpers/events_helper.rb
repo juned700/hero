bbc2432410f472b692731a8bc6b94610cd5901eb
Rahul <rahul@gmail.com>
2019-12-02 14:46:07 +0530

    use some helpers and css style on event list page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def event_price(price)
+    if price.blank? || price.zero?
+      'Free'
+    else
+      number_to_currency(price, unit: 'RS ')
+    end
+  end
+
+  def event_date(date)
+    return date.strftime('%d/%B/%Y : %H:%M %p') if date 
+    'Comming Soon'
+  end
 end
