e95d57a928c627c793ade28b6b9e4642b4285bd0
Rahul <rahul@gmail.com>
2019-12-04 13:03:44 +0530

    make the show routes as a dynamic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,6 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(1)
+    @event = Event.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/events" => "events#index"
-  get "/events/1" => "events#show"
+  get "/events/:id" => "events#show"
 end
