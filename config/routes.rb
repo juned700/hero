9a2e72b8b79071c553210c08f0ac94a82ca0876f
Rahul <rahul@gmail.com>
2019-12-04 13:06:10 +0530

    link index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/events" => "events#index"
-  get "/events/:id" => "events#show"
+  get "/events" => "events#index", as: :events
+  get "/events/:id" => "events#show", as: :event
 end
