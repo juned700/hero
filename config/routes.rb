608c623cd75114048dd98f595c7c85fa6316974e
Rahul <rahul@gmail.com>
2019-12-09 12:42:17 +0530

    added create udpate and detroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/events" => "events#index", as: :events
+  get "/events/new" => "events#new", as: :new_event
   get "/events/:id" => "events#show", as: :event
+  get "/events/:id/edit" => "events#edit", as: :edit_event
+  patch "/events/:id" => "events#update"
+  post '/events' => "events#create"
+  delete "/events/:id" => "events#destroy", as: :destroy_event
 
   root :to => "home#index"
 end
