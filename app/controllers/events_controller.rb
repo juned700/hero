008e9739b6a77b60c1b6c60242a041143a621687
Rahul <rahul@gmail.com>
2019-12-09 17:30:54 +0530

    app/controllers/events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
-  #before_action :get_event, only: [:show, :edit, :update, :destroy]
-  before_action :get_event, excpet: [:new, :create, :index]
+  before_action :get_event, only: [:show, :edit, :update, :destroy]
+  #before_action :get_event, excpet: [:new, :create, :index]
 
   def index
     @events = Event.all
