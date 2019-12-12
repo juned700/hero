6bef1b8b2ca8a1ef720cc433dd4a72c87cfec51c
Rahul <rahul@gmail.com>
2019-12-12 15:13:30 +0530

    added has_many asociation in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  has_many :registrations
+  
   validates :name, :place, :event_date, :spot, :image_file_name, presence: true
 
   validates :spot, numericality: { only_integer: true }
